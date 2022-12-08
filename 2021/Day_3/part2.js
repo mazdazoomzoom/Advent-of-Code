7cc9c2dc1fcd11ad9235b8d9783bdcfd1508fd99
James Reinhart <james.reinhart90@gmail.com>
2022-12-08 14:28:18 +0000

    Day 7 and 8

--- 2021/Day_3/part2.js
+++ 2021/Day_3/part2.js
@@ -12,37 +12,63 @@ const getInput = async () => {
 const main = async () => {
 	try {
 		let inputData = await getInput();
-		let oxGenRating = [...inputData];
-		let co2ScrubRating = [...inputData];
+		let oxGenRating = getOxGenRating(inputData);
+		let co2ScrubRating = getCO2ScrubRating(inputData);
 
+		console.log(`Oxygen Generator Rating: ${oxGenRating}`);
+		console.log(`CO2 Scrubber Rating: ${co2ScrubRating}`);
+		console.log(`Total Rating: ${oxGenRating * co2ScrubRating}`);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const getOxGenRating = (inputData) => {
+	while (inputData.length > 1) {
 		for (let col = 0; col < inputData[0].length; col++) {
+			if (inputData.length == 1) {
+				break;
+			}
+
 			let count0 = 0;
 			let count1 = 0;
 			for (let row of inputData) {
 				let bit = row.charAt(col);
-				if (bit == 0) count0++;
-				if (bit == 1) count1++;
+				bit == 0 ? count0++ : count1++;
 			}
 
 			if (count0 > count1) {
-				if (oxGenRating.length > 1) oxGenRating = oxGenRating.filter((value) => value.charAt(col) == 0);
-				if (co2ScrubRating.length > 1)
-					co2ScrubRating = co2ScrubRating.filter((value) => value.charAt(col) == 1);
-			} else if (count0 < count1) {
-				if (oxGenRating.length > 1) oxGenRating = oxGenRating.filter((value) => value.charAt(col) == 1);
-				if (co2ScrubRating.length > 1)
-					co2ScrubRating = co2ScrubRating.filter((value) => value.charAt(col) == 0);
+				inputData = inputData.filter((value) => value.charAt(col) == 0);
 			} else {
-				if (oxGenRating.length > 1) oxGenRating = oxGenRating.filter((value) => value.charAt(col) == 1);
-				if (co2ScrubRating.length > 1)
-					co2ScrubRating = co2ScrubRating.filter((value) => value.charAt(col) == 0);
+				inputData = inputData.filter((value) => value.charAt(col) == 1);
 			}
 		}
-
-		console.log(parseInt(oxGenRating[0], 2) * parseInt(co2ScrubRating[0], 2));
-	} catch (error) {
-		console.error(error);
 	}
+	return parseInt(inputData[0], 2);
+};
+
+const getCO2ScrubRating = (inputData) => {
+	while (inputData.length > 1) {
+		for (let col = 0; col < inputData[0].length; col++) {
+			if (inputData.length == 1) {
+				break;
+			}
+
+			let count0 = 0;
+			let count1 = 0;
+			for (let row of inputData) {
+				let bit = row.charAt(col);
+				bit == 0 ? count0++ : count1++;
+			}
+
+			if (count0 > count1) {
+				inputData = inputData.filter((value) => value.charAt(col) == 1);
+			} else {
+				inputData = inputData.filter((value) => value.charAt(col) == 0);
+			}
+		}
+	}
+	return parseInt(inputData[0], 2);
 };
 
 main();
---
+++ 2022/Day_7/part2.js
@@ -0,0 +1,154 @@
+const fs = require('fs');
+
+const getInput = async () => {
+	try {
+		const data = fs.readFileSync('./input.txt', 'utf8');
+		return data.split('\n');
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+const main = async () => {
+	try {
+		let inputData = await getInput();
+
+		// Let's create a folder structure from input data
+		// Within the terminal output, lines that begin with $ are commands you executed, very much like some modern computers:
+		// cd means change directory. This changes which directory is the current directory, but the specific result depends on the argument:
+		// 		cd x moves in one level: it looks in the current directory for the directory named x and makes it the current directory.
+		// 		cd .. moves out one level: it finds the directory that contains the current directory, then makes that directory the current directory.
+		// 		cd / switches the current directory to the outermost directory, /.
+		// ls means list. It prints out all of the files and directories immediately contained by the current directory:
+		// 		123 abc means that the current directory contains a file named abc with size 123.
+		// 		dir xyz means that the current directory contains a directory named xyz.
+
+		// Directory structure
+		// {
+		// 	{
+		//		name: '/',
+		// 		files: [],
+		// 		directories: [],
+		// 		parent: null,
+		// 		size: 0,
+		// }
+		// File structure
+		// {
+		// 		name: 'abc',
+		// 		size: 123
+		// 	}
+
+		// Let's create a file structure
+		let fileDirectoryStructure = {
+			name: '/',
+			files: [],
+			directories: [],
+			parent: null,
+			size: 0,
+		};
+
+		let currentWorkingDirectory = fileDirectoryStructure['/'];
+
+		for (let i = 0; i < inputData.length; i++) {
+			let line = inputData[i];
+			let lineParts = line.split(' ');
+
+			// Check if the line is a command
+			if (lineParts[0] === '$') {
+				// Check if the command is cd
+				if (lineParts[1] === 'cd') {
+					// Check if the argument is a directory
+					if (lineParts[2] === '/') {
+						// This is the root directory
+						currentWorkingDirectory = fileDirectoryStructure;
+					} else if (lineParts[2] === '..') {
+						// This is the parent directory
+						currentWorkingDirectory = currentWorkingDirectory.parent;
+					} else {
+						// Navigate to the directory specified
+						// Check if the directory exists
+						if (currentWorkingDirectory.directories[lineParts[2]] === undefined) {
+							// If it doesn't exist, create it
+							currentWorkingDirectory.directories.push({
+								name: lineParts[2],
+								files: [],
+								directories: [],
+								parent: currentWorkingDirectory,
+								size: 0,
+							});
+						}
+						currentWorkingDirectory = currentWorkingDirectory.directories.find((directory) => directory.name === lineParts[2]);
+					}
+				}
+			} else {
+				// If the line is not a command, it is a file or directory
+				// Check if the line is a directory
+				if (lineParts[0] === 'dir') {
+					// Check if the directory exists
+					if (fileDirectoryStructure[lineParts[1]] === undefined) {
+						// If it doesn't exist, create it
+						currentWorkingDirectory.directories.push({
+							name: lineParts[1],
+							files: [],
+							directories: [],
+							parent: currentWorkingDirectory,
+						});
+					}
+				} else {
+					// The line is a file
+					currentWorkingDirectory.files.push({
+						name: lineParts[1],
+						size: parseInt(lineParts[0]),
+					});
+				}
+			}
+		}
+
+		// Now that we have a file structure
+		// Let's calculate the size of each directory
+		let calculateSize = (directory) => {
+			let size = 0;
+			for (let i = 0; i < directory.files.length; i++) {
+				size += directory.files[i].size;
+			}
+			for (let i = 0; i < directory.directories.length; i++) {
+				size += calculateSize(directory.directories[i]);
+			}
+			directory.size = size;
+			return size;
+		};
+		calculateSize(fileDirectoryStructure);
+
+		let spaceNeeded = 30000000;
+		let usedSpace = fileDirectoryStructure.size;
+		let freeSpace = 70000000 - usedSpace;
+		let spaceToFreeUp = spaceNeeded - freeSpace;
+
+		// Now that we have the size of each directory
+		// We need to size of a directory that is the smallest in size that is greater than the space to free up
+		let smallestDirectorySize = 0;
+		let smallestDirectory = null;
+		let findSmallestDirectory = (directory) => {
+			if (directory.size > spaceToFreeUp) {
+				if (smallestDirectorySize === 0) {
+					smallestDirectorySize = directory.size;
+					smallestDirectory = directory;
+				} else if (directory.size < smallestDirectorySize) {
+					smallestDirectorySize = directory.size;
+					smallestDirectory = directory;
+				}
+			}
+			for (let i = 0; i < directory.directories.length; i++) {
+				findSmallestDirectory(directory.directories[i]);
+			}
+		};
+		findSmallestDirectory(fileDirectoryStructure);
+
+		// Log the smallest directory and its size
+		console.log(smallestDirectory.name, smallestDirectory.size);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+main();
